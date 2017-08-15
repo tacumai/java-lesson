f279444c7fa7b7b03907c37ca003ba75fed28e93
tacmac <tacuma.igei@gmail.com>
2017-08-16 00:15:28 +0900

    rm unused field

--- Combatant.java
+++ Combatant.java
@@ -1,12 +1,10 @@
 public abstract class Combatant {
   String name;
   int hp;
-  int basicDamegeScore;
 
   Combatant(String name) {
     this.name = name;
     this.hp = 100;
-    this.basicDamegeScore = 10;
   }
 
   public abstract void tackle(Monster m);
