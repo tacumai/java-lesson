f78a3950d84d022466b17c7f1b8e2e0ad4dca9b0
tacumai <tacuma.igei@gmail.com>
2017-08-23 14:39:13 +0900

    test commit

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args){
+    // Test
     System.out.println("Game started...!");
     Hero yoshihiko = new Hero("よしひこ");
     Wizard anna = new Wizard("あんな");
